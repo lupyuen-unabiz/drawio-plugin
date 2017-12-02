b1cc822a0437265e81d83d4ef6986b762e00513d
lupyuen-unabiz <ly.lee@unabiz.com>
2017-12-02 18:45:53 +0800

    First version

---
+++ data.js
@@ -0,0 +1,14 @@
+{
+  "compilerOptions": {
+    "allowJs": true,
+    "module": "es6",
+    "target": "es5",
+    "sourceMap": true
+  },
+  "include": [
+    "./mxgraph/"
+  ],
+  "exclude": [
+    "node_modules"
+  ]
+}
