926379235cc033585a520060d7fceda8a815e698
lupyuen-unabiz <ly.lee@unabiz.com>
2017-12-02 18:53:47 +0800

    First version

--- plugin.js
+++ plugin.js
@@ -16,7 +16,7 @@ function fetchData() {
     r.onreadystatechange = function () {
         if (r.readyState != 4 || r.status != 200)
             return;
-        alert("Success: " + r.responseText);
+        console.log("Success: " + r.responseText);
     };
     // r.send("banana=yellow");
     r.send();
