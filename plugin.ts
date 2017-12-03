26442ae290b12d0aa446a35b9bf73200816ef1da
lupyuen-unabiz <ly.lee@unabiz.com>
2017-12-03 19:44:02 +0800

    chartjs renders OK

--- plugin.ts
+++ plugin.ts
@@ -68,12 +68,12 @@ function initChart(canvas: HTMLCanvasElement): void {
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(255, 206, 86, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+          'rgba(153, 102, 255, 0.8)',
+          'rgba(255, 159, 64, 0.8)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
